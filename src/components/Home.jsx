d2007ddda1eec488773825c68b50960256175070
RicardoSalazarR <axel.111yo@gmail.com>
2022-12-23 23:55:12 -0600

    ready to use

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,15 @@ const Home = () => {
             <div className="title-container">
                 <h1>Web Developer</h1>
                 <div className="icons">
-                    <box-icon type='logo' name='linkedin' color='white'></box-icon>
-                    <box-icon name='spreadsheet' type='solid' color='white'></box-icon>
-                    <box-icon name='gmail' type='logo' color='white'></box-icon>
+                    <a href="https://www.linkedin.com/in/ricardo-salazar-b9bb13227/" target='_blank'>
+                        <box-icon type='logo' name='linkedin' color='white' href='www.google.com'></box-icon>
+                    </a>
+                    <a href="https://docs.google.com/document/d/1rZ0IJ7-WM802UDXY2LakzOQVkmKdJZOH6rTkDQof9MM/edit?usp=sharing" target='_blank'>
+                        <box-icon name='spreadsheet' type='solid' color='white' href=''></box-icon>
+                    </a>
+                    <a href="mailto:axel.111yo@gmail.com?Subject=Portfolio" target='_blank'>
+                        <box-icon name='gmail' type='logo' color='white'></box-icon>
+                    </a>
                 </div>
             </div>
             <div className='img-container'>
